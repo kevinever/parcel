6a3cd6bef16a3907ba89772a49940e861232ab53
kevinever <nyayihakevin@gmail.com>
2018-11-07 20:04:49 +0200

    pushing the features of POST and GET

--- README.md
+++ README.md
@@ -1,30 +1,41 @@
-# 'send it' is a Courier Management System
+# 'send it' is a parcel or a Courier Management System
 
 ## Purpose
 
-To be able to create an order view order, delete order
+to enable users to :
+
+                     1.Create a parcel delivery order 
+                     2.Get all parcel delivery orders
+                     3.Get a specific parcel delivery order 
+                     4.Cancel or delete a parcel delivery order 
 ## Installation
 
-Clone or download the repository(parcel), then from the command line, run `npm install` and `node run app.js`. The system should open in a new browser window or tab automatically at <http://localhost:1000>.
+Clone or download the repository(parcel), then from the command line, run `npm init` and change directory bin and run`node www`. The system should open in a new browser window or tab automatically at <http://localhost:7000> or you can even set your default port from www server file.
 
+
+also you going to need to install handlebars in other to work with routing and also html rendering
 ## Technologies Used
 
 **Back end:**
 
 * Node.js
-* Express
+* Express(handlerbars)
 
 **Front end:**
 
 *html
 *css
 
+**HTML RENDERING ENGINE**
+
+*handlerbars(.hbs)
+
 **Testing:**
 
 * Mocha
 
-## API
+
 
 Endpoints for couriers.
 
-## Bundles: Proposed Front-End Treatment
+##N.B thiss version is still under development some features may have not yet to be implemented.
---
+++ routes/index.js
@@ -0,0 +1,107 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Send it with us!', condition: true, anyArray: [1,2,3] });
+});
+
+router.get('/test/:Shippername', function(req, res, next) {
+  res.render('test', {output: req.params.Shippername});
+});
+
+router.get('/test/:Shipperphone', function(req, res, next) {
+  res.render('test', {output: req.params.Shipperphone});
+});
+
+router.get('/test/:Shipperaddress', function(req, res, next) {
+  res.render('test', {output: req.params.Shipperaddress});
+});
+
+
+router.get('/test/:Receivername', function(req, res, next) {
+  res.render('test', {output: req.params.Receivername});
+});
+
+
+router.get('/test/:Receiverphone', function(req, res, next) {
+  res.render('test', {output: req.params.Receiverphone});
+});
+
+
+
+router.get('/test/:Receiveraddress', function(req, res, next) {
+  res.render('test', {output: req.params.Receiveraddress});
+});
+
+router.get('/test/:ConsignmentNo', function(req, res, next) {
+  res.render('test', {output: req.params.ConsignmentNo});
+});
+
+router.get('/test/:Shiptype', function(req, res, next) {
+  res.render('test', {output: req.params.Shiptype});
+});
+
+router.get('/test/:Weight', function(req, res, next) {
+  res.render('test', {output: req.params.Weight});
+});
+
+
+
+router.get('/test/:Invoiceno', function(req, res, next) {
+  res.render('test', {output: req.params.Invoiceno});
+});
+
+
+
+router.get('/test/:Qnty', function(req, res, next) {
+  res.render('test', {output: req.params.Qnty});
+});
+
+
+
+router.get('/test/:Bookingmode', function(req, res, next) {
+  res.render('test', {output: req.params.Bookingmode});
+});
+
+router.get('/test/:Totalfreight', function(req, res, next) {
+  res.render('test', {output: req.params.Totalfreight});
+});
+
+router.get('/test/:Mode', function(req, res, next) {
+  res.render('test', {output: req.params.Mode});
+});
+
+router.get('/test/:Depttime', function(req, res, next) {
+  res.render('test', {output: req.params.Depttime});
+});
+
+router.get('/test/:Destination', function(req, res, next) {
+  res.render('test', {output: req.params.Destination});
+});
+
+router.get('/test/:Packupdate', function(req, res, next) {
+  res.render('test', {output: req.params.Packupdate});
+});
+
+
+
+router.get('/test/:Pickuptime', function(req, res, next) {
+  res.render('test', {output: req.params.Pickuptime});
+});
+
+
+router.get('/test/:Pickuptime', function(req, res, next) {
+  res.render('test', {output: req.params.Pickuptime});
+});
+
+
+router.get('/test/:Comments', function(req, res, next) {
+  res.render('test', {output: req.params.Comments});
+});
+
+router.post('/test/submit', function(req, res, next) {
+  res.redirect('/test/...');
+});
+
+module.exports = router;
